eedf63e1d724f494bc3f4a2ebf6e8d6e257cf6be
Anastasia_Liska <agni-0808@yandex.ru>
2022-11-01 17:11:12 +0300

    master

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Int32.Parse(Console.ReadLine());
+
+while(num > 2)
+{
+if(num % 2 == 0)
+{
+   Console.Write(num + ",");    
+}
+    num--;
+}
+if(num == 2)
+{
+   Console.WriteLine("2 ");
+}
+else 
+{ 
+   Console.Write("Нет чётных чисел ");
+}
